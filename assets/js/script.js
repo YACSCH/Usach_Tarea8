03cea30b5303ecddff1c744239ac4fd95b698e66
Yamil Castillo <yamilcs@gmail.com>
2023-07-03 23:53:52 -0400

    subiendo tarea 8 a git

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,53 @@
-let listadoPokemones = document.querySelector("#listadoPokemones");
-let url = "https://pokeapi.co/api/v2/pokemon/";
+let listadoPersonajes = document.querySelector("#listadoPersonajes");
+let url = "https://rickandmortyapi.com/api/character";
 
-const fetchData = (async () => {
-    try {
-        for(let i = 1; i <= 20;i++){
-            const response = await fetch(url+i)
-            const data = await response.json()
-            mostrarPokemon(data) 
-        }
-    } catch (error) {
-        console.log(error)
+class Character {
+    constructor(name, species, image) {
+      this._name = name;
+      this._species = species;
+      this._image = image;
     }
-})()
+  
+    get name() {
+      return this._name;
+    }
+  
+    get species() {
+      return this._species;
+    }
+  
+    get image() {
+      return this._image;
+    }
+  
+    renderizarPersonajes() {
 
-
-function mostrarPokemon(pokemon) {
     const div = document.createElement("div");
-    div.classList.add("pokemon") 
-    div.innerHTML = `
-            <img src="${pokemon.sprites.other["official-artwork"].front_default}" alt="${pokemon.name}"/>
-            <h2 class="pokemon-nombre">${pokemon.name}</h2>`
-    listadoPokemones.append(div)
-}
+      div.classList.add("personaje") 
+      div.innerHTML = `
+              <img src="${this.image}" alt=""/>
+              <h2 class="personaje-nombre">${this.name}</h2>
+              <h3 class="personaje-especie">${this.species}</h3>`
+
+      listadoPersonajes.append(div)
+    }
+  }
+  
+  async function fetchPersonajes() {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      const characters = data.results;
+  
+      characters.forEach(character => {
+        const { name, species, image } = character;
+        const newCharacter = new Character(name, species, image);
+        newCharacter.renderizarPersonajes();
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  fetchPersonajes();
+  
